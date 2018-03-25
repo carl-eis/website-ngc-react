6e75d99a2a2327f9f5c8fbf7c8bf66f556267f2f
Carl Eiserman <carleiserman@gmail.com>
2018-03-25 22:59:12 +0200

    fix: linting errors

--- src/atoms/media-queries/index.js
+++ src/atoms/media-queries/index.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import { css } from 'styled-components';
 
 // these sizes are arbitrary and you can set them to whatever you wish
 
@@ -14,6 +14,7 @@ const media = Object.keys(sizes).reduce((accumulator, label) => {
   // use em in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
   const emSize = sizes[label] / 16;
+  // eslint-disable-next-line
   accumulator[label] = (...args) => css`
     @media (max-width: ${emSize}em) {
       ${css(...args)}
