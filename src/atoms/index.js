18b21f7e93a420c54caf72d3fbdf34d972ac7685
Carl Eiserman <carleiserman@gmail.com>
2018-03-25 22:58:19 +0200

    feat: added media queries

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,2 +1,3 @@
 /* eslint import/prefer-default-export: 0 */
 export { default as ContentContainer } from './content-container';
+export { default as MediaQUeries } from './media-queries';
---
+++ src/atoms/media-queries/index.js
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components';
+
+// these sizes are arbitrary and you can set them to whatever you wish
+
+const sizes = {
+  giant: 1200,
+  desktop: 992,
+  tablet: 768,
+  phone: 480,
+};
+
+// iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+  const emSize = sizes[label] / 16;
+  accumulator[label] = (...args) => css`
+    @media (max-width: ${emSize}em) {
+      ${css(...args)}
+    }
+  `;
+  return accumulator;
+}, {});
+
+export default media;
