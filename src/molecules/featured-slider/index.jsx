987e4d2765f559b7049ccca483b9321d6bf54abb
Carl Eiserman <carleiserman@gmail.com>
2018-03-27 23:30:48 +0200

    feat: added unique ids to feature slider

--- src/molecules/featured-slider/index.jsx
+++ src/molecules/featured-slider/index.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-// import { generate } from 'shortid';
+import { generate } from 'shortid';
 import {
   jarallax,
-  jarallaxElement,
-  jarallaxVideo,
+  // jarallaxElement,
+  // jarallaxVideo,
 } from 'jarallax';
 
 import {
@@ -17,20 +17,44 @@ import {
   SliderContent,
 } from '../styles';
 
+declare var $: any;
+
 /* eslint-disable react/prefer-stateless-function */
 class FeaturedSlider extends Component {
+  state = {
+    id: `default-jarallax-${generate()}`,
+    infoId: `default-info-fade-${generate()}`,
+  };
+
+  componentWillMount() {
+    const uniqueClassId = `jarallax-${generate()}`;
+    const infoId = `infobox-${generate()}`;
+    this.setState({ id: uniqueClassId, infoId });
+  }
+
   componentDidMount() {
-    jarallax(document.querySelector('.jarallax-feature-slider'), {
+    jarallax(document.querySelector(`.${this.state.id}`), {
       speed: 0.2,
       disableParallax: /iPad|iPhone|iPod|Android/,
       disableVideo: /iPad|iPhone|iPod|Android/,
     });
+
+    const infoId = `.${this.state.infoId}`;
+
+    // const infoBox = $(infoId).offset(infoId.height).top;
+    // $(window).scroll(() => {
+    //   if ((infoBox - $(window).scrollTop()) < 20) {
+    //     $(infoId).stop().fadeTo(100, 0);
+    //   } else {
+    //     $(infoId).stop().fadeTo('fast', 1);
+    //   }
+    // });
   }
 
   render() {
     const {
-      title1,
-      title2,
+      title,
+      subtitle,
       quote,
       buttonText,
       buttonClick,
@@ -41,16 +65,16 @@ class FeaturedSlider extends Component {
     return (
       <SliderContainer>
         <SliderImageContainer data-qa="slider-image-container">
-          <div className="jarallax-feature-slider">
+          <div className={this.state.id}>
             <img src={backgroundImage} className="jarallax-img" alt="" />
           </div>
 
         </SliderImageContainer>
         <SliderContentContainer data-qa="slider-content-container">
-          <div className="container">
+          <div className={`container ${this.state.infoId}`}>
             <SliderContent data-qa="slider-content">
-              <h1>{title1}</h1>
-              <h1>{title2}</h1>
+              <h1>{title}</h1>
+              <h1>{subtitle}</h1>
               <p>{quote}</p>
               <button className="btn btn-primary" onClick={buttonClick}>
                 {buttonText}
@@ -66,8 +90,8 @@ class FeaturedSlider extends Component {
 /* eslint-enable react/prefer-stateless-function */
 
 FeaturedSlider.propTypes = {
-  title1: string,
-  title2: string,
+  title: string,
+  subtitle: string,
   quote: string,
   buttonText: string,
   buttonClick: func,
@@ -76,8 +100,8 @@ FeaturedSlider.propTypes = {
 };
 
 FeaturedSlider.defaultProps = {
-  title1: 'Default Title 1',
-  title2: 'Default Title 2',
+  title: 'Site Title',
+  subtitle: 'Default Title 2',
   quote: 'Default quote',
   buttonText: 'Click Me',
   buttonClick: () => { },
